fc3b09dbc58aa1f83ba8cb36966c73d4563d9aaf
MaouDabi <erkipandial@gmail.com>
2025-06-25 12:26:26 +0700

    Update prgM.js

--- prgM.js
+++ prgM.js
@@ -1,38 +1,38 @@
-const https = require('https')
-const vm = require('vm')
+const https = require('https');
+const vm = require('vm');
 
-const load = url => new Promise((res, rej) => {
-  https.get(url, r => {
-    let d = ''
-    r.on('data', c => d += c)
-    r.on('end', () => {
+const load = url => new Promise((resolve, reject) => {
+  https.get(url, res => {
+    let data = '';
+    res.on('data', chunk => data += chunk);
+    res.on('end', () => {
       try {
-        const s = new vm.Script(d, { filename: 'CrckMD.js' })
-        const ctx = vm.createContext({ module: {}, exports: {} })
-        s.runInContext(ctx)
-        res(ctx.module.exports || ctx.exports)
-      } catch (e) {
-        rej(e)
+        const script = new vm.Script(data, { filename: 'CrckMD.js' });
+        const context = vm.createContext({ module: {}, exports: {} });
+        script.runInContext(context);
+        resolve(context.module.exports || context.exports);
+      } catch (err) {
+        reject(new Error('Gagal menjalankan script VM: ' + err.message));
       }
-    })
-  }).on('error', rej)
-})
+    });
+  }).on('error', err => reject(new Error('Gagal mengambil URL: ' + err.message)));
+});
 
 module.exports = async (conn, msg, txt) => {
-  if (!txt || !txt.startsWith(global.isPrefix + '/')) return false
+  if (!txt || !txt.startsWith(global.isPrefix + '/')) return false;
 
-  const q = txt.slice((global.isPrefix + '/').length).trim()
-  const m = q.match(/^CrackMode\s*:\s*-r=\s*{\s*(.+?)\s*}$/i)
-  if (!m) return false
+  const q = txt.slice((global.isPrefix + '/').length).trim();
+  const m = q.match(/^CrackMode\s*:\s*-r=\s*{\s*(.+?)\s*}$/i);
+  if (!m) return false;
 
-  const info = { chatId: msg.chat, sender: msg.sender, fromMe: msg.key.fromMe }
+  const info = exCht(msg);
 
   try {
-    const url = 'https://raw.githubusercontent.com/username/repo/branch/toolkit/CrckMD.js'
-    const fn = await load(url)
-    return await fn(conn, msg, info, q, m)
-  } catch (e) {
-    console.error('[CRACK_HANDLER]', e)
-    return false
+    const url = 'https://raw.githubusercontent.com/username/repo/branch/toolkit/CrckMD.js';
+    const fn = await load(url);
+    return await fn(conn, msg, info, q, m);
+  } catch (err) {
+    console.error('[CRACK_HANDLER]', err);
+    return false;
   }
-}
+};
