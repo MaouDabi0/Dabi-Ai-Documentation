0b860115e33e2a20aeb2bc46fdaceafb5faedc49
MaouDabi <erkipandial@gmail.com>
2025-11-25 06:23:12 +0700

    Update func.js

--- assets/func.js
+++ assets/func.js
@@ -2,3 +2,7 @@ import fs from 'fs'
 
 const sifatList = [ 'Baik', 'Jahat', 'Lucu', 'Pemarah', 'Penyabar', 'Pemalu', 'Percaya Diri', 'Pemberani', 'Cengeng', 'Bijaksana', 'Pintar', 'Sombong', 'Rendah Hati', 'Setia', 'Cemburuan', 'Pelit', 'Dermawan', 'Pemalas', 'kek kontol', 'Rajin', 'Sensitif'
 ]
+
+export default {
+  sifatList
+}
